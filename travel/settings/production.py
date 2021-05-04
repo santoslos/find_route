ba81899553b370cb6b275b508eebc47c38575adf
santoslos <papoian.gosha@gmail.com>
2021-05-04 22:07:00 +0300

    create templates errors

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -130,5 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
